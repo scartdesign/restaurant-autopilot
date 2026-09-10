ba6bcbdd6cfd87adcb236fe8fb0a0110edbd7706
scartdesign <146715641+scartdesign@users.noreply.github.com>
2026-09-10 11:45:43 +0200

    Load production sales styles

--- src/main.tsx
+++ src/main.tsx
@@ -15,6 +15,7 @@ import './brand-kit.css'
 import './onboarding-brand.css'
 import './schedule-demo.css'
 import './sales.css'
+import './sales-v2.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
