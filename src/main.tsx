5080a447551765b91a328e401e01bbc21d2497b0
scartdesign <146715641+scartdesign@users.noreply.github.com>
2026-09-10 02:05:07 +0200

    Load final UI polish layer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './premium.css'
+import './final.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
