d4357602afa3abb7cde16327b85a5772175fe442
scartdesign <146715641+scartdesign@users.noreply.github.com>
2026-09-10 02:49:35 +0200

    Load Visual Studio styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './premium.css'
 import './final.css'
+import './studio.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
